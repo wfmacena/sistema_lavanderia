2039d6ba2ac6cd37817fe28135dc10404f4ab7e2
wmcod <wmacenq@gmail.com>
2026-03-16 00:22:58 -0300

    implementa CRUD manual de clientes

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaLavanderia.Data;
+using SistemaLavanderia.Models;
+
+namespace SistemaLavanderia.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly LavanderiaContext _context;
+
+        public ClientesController(LavanderiaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Clientes.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Cliente cliente)
+        {
+            if (id != cliente.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cliente = await _context.Clientes.FirstOrDefaultAsync(c => c.Id == id);
+            return View(cliente);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            _context.Clientes.Remove(cliente);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Views/Clientes/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SistemaLavanderia.Views.Clientes
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
