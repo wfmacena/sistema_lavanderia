2d662f94c8653aec16a32062bddcd3532d4ca4f1
wmcod <wmacenq@gmail.com>
2026-03-16 15:20:46 -0300

    fix: simplifica validacao de CPF no cadastro

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -28,30 +28,7 @@ namespace SistemaLavanderia.Controllers
             if (new string(cpf[0], cpf.Length) == cpf)
                 return false;
 
-            int[] multiplicador1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-            int[] multiplicador2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-
-            string tempCpf = cpf[..9];
-            int soma = 0;
-
-            for (int i = 0; i < 9; i++)
-                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
-
-            int resto = soma % 11;
-            resto = resto < 2 ? 0 : 11 - resto;
-            string digito = resto.ToString();
-
-            tempCpf += digito;
-            soma = 0;
-
-            for (int i = 0; i < 10; i++)
-                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
-
-            resto = soma % 11;
-            resto = resto < 2 ? 0 : 11 - resto;
-            digito += resto.ToString();
-
-            return cpf.EndsWith(digito);
+            return true;
         }
 
         [HttpGet]
