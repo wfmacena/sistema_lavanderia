59011061e431f2b39abbf4900570575cddc17c64
wmcod <wmacenq@gmail.com>
2026-03-17 15:03:16 -0300

    feat: implementações de melhorias nav link, pagina welcome

---
+++ Models/ServicoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaLavanderia.Models
+{
+    public class ServicoViewModel
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public decimal PrecoBase { get; set; }
+        public string UnidadeMedida { get; set; } = string.Empty;
+    }
+}
